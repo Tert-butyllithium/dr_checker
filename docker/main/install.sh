7702feaffb4a0d97ac522f03e6c46d087ebe586b
Haonan Li <lihaonan1105@gmail.com>
2022-11-14 12:21:10 -0800

    some update to docker

--- docker/main/install.sh
+++ docker/main/install.sh
@@ -2,7 +2,7 @@
 source /etc/bash_completion
 mkdir /dr_checker
 cd /dr_checker
-git clone https://github.com/ucsb-seclab/dr_checker.git gitrepo
+git clone https://github.com/Tert-butyllithium/dr_checker.git gitrepo
 cd gitrepo/helper_scripts
 python setup_drchecker.py -o /dr_checker/drcheckerdeps
 echo 'export LLVM_ROOT=/dr_checker/drcheckerdeps/llvm/build' >> ~/.bashrc
